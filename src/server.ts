37d43bc2fdbafb41a1f503524d6ede81b275f2d5
Abel Souza <abelsouzacosta@gmail.com>
2021-12-24 09:16:03 -0300

    refactor(debug): allows to use vscode debugger
    Changes to use vscode debugger feature

--- src/server.ts
+++ src/server.ts
@@ -4,10 +4,18 @@ const app = express();
 
 const PORT = 3333;
 
+app.use(express.json());
+
 app.get("/", (request: Request, response: Response) => {
   return response.status(200).json({
     message: "Olá mundo",
   });
+});
+
+app.post("/courses", (request, response) => {
+  const { name } = request.body;
+
+  return response.status(200).json({ name });
 });
 
 app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
