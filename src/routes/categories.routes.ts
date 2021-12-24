01e8c91b1cdd62023501a522777be64175fef1d8
Abel Souza <abelsouzacosta@gmail.com>
2021-12-24 18:10:51 -0300

    feat(categories): list
    Allows to list all categories inserted in the "database"

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,29 +1,22 @@
 import { Router, Request, Response } from "express";
 
-import { Category } from "../models/Category";
+import { CategoryRepository } from "../repositories/CategoriesRepository";
 
 const categoriesRoutes = Router();
-
-const categories: Category[] = [];
+const categoryRepository = new CategoryRepository();
 
 categoriesRoutes.post("/", (request: Request, response: Response) => {
   const { name, description } = request.body;
 
-  const category = new Category();
-
-  Object.assign(category, {
-    name,
-    description,
-    createdAt: new Date(),
-  });
-
-  categories.push(category);
+  categoryRepository.create({ name, description });
 
   return response.status(201).send();
 });
 
 categoriesRoutes.get("/", (request: Request, response: Response) => {
-  return response.status(200).json(categories);
+  const list = categoryRepository.list();
+
+  return response.status(200).json(list);
 });
 
 export { categoriesRoutes };
