187d2ded51249b2b1674331d55085b9f39c57246
Abel Souza <abelsouzacosta@gmail.com>
2021-12-24 08:09:19 -0300

    chore: adding script

--- src/server.ts
+++ src/server.ts
@@ -5,7 +5,9 @@ const app = express();
 const PORT = 3333;
 
 app.get("/", (request: Request, response: Response) => {
-  return response.status(200).send("Hello World");
+  return response.status(200).json({
+    message: "Olá mundo",
+  });
 });
 
 app.listen(PORT, () => console.log(`Server listen on port ${PORT}`));
