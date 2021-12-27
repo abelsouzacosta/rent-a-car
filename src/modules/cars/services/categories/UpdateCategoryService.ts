dba84d70144d55e79af54f335efad4095cc7d6b2
Abel Souza <abelsouzacosta@gmail.com>
2021-12-27 20:04:37 -0300

    fix(categories): update
    Not allows to update a category name to a name of other instance

--- src/modules/cars/services/categories/UpdateCategoryService.ts
+++ src/modules/cars/services/categories/UpdateCategoryService.ts
@@ -13,7 +13,12 @@ class UdpateCategoryService {
   execute({ id, name, description }: IUpdateCategoryDTO): void {
     const category = this.repository.findById(id);
 
+    const foundCategoryByName = this.repository.findByName(name);
+
     if (!category) throw new Error("Category not found");
+
+    if (foundCategoryByName && foundCategoryByName.id !== category.id)
+      throw new Error(`There's already an category with the given name`);
 
     this.repository.update({ id, name, description });
   }
