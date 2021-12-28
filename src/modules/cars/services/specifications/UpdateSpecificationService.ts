9fc35c243e7064d850b6ac571a83648ac81a05f1
Abel Souza <abelsouzacosta@gmail.com>
2021-12-27 22:43:03 -0300

    fix(specifications): update
    Not allows to update a specification name to a name of another specification instance

--- src/modules/cars/services/specifications/UpdateSpecificationService.ts
+++ src/modules/cars/services/specifications/UpdateSpecificationService.ts
@@ -13,6 +13,14 @@ class UpdateSpecificationService {
   execute({ id, name, description }: IUpdateSpecificationDTO): void {
     const specification = this.repository.findById(id);
 
+    const foundSpecificationByName = this.repository.findByName(name);
+
+    if (
+      foundSpecificationByName &&
+      foundSpecificationByName.id !== specification.id
+    )
+      throw new Error(`There's already an specification with the given name`);
+
     if (!specification) throw new Error("Specification not found");
 
     this.repository.update({ id, name, description });
