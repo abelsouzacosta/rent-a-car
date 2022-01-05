f42b9cbf1c9c984250803a4d602f2edf51612819
Abel Souza <abelsouzacosta@gmail.com>
2022-01-05 00:49:28 -0300

    feat(category): import
    Allows to import categories from a upload of a csv file

--- src/modules/cars/usecases/categories/importCategory/ImportCategoryUseCase.ts
+++ src/modules/cars/usecases/categories/importCategory/ImportCategoryUseCase.ts
@@ -1,6 +1,17 @@
+import { parse } from "csv-parse";
+import { createReadStream } from "fs";
+
 class ImportCategoryUseCase {
   execute(file: Express.Multer.File): void {
-    console.log(file);
+    const stream = createReadStream(file.path);
+
+    const parseFile = parse({});
+
+    stream.pipe(parseFile);
+
+    parseFile.on("data", async (line) => {
+      console.log(line);
+    });
   }
 }
 
