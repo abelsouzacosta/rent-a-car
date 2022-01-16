690c4ec66c388e49c0146cc3670d35da4a2a8db8
Abel Souza <abelsouzacosta@gmail.com>
2022-01-16 01:04:17 -0300

    refactor(specifications): model
    refactor to transform the specifications model into an typeorm entity

--- src/modules/cars/entities/Specification.ts
+++ src/modules/cars/entities/Specification.ts
@@ -1,10 +1,19 @@
+import { Column, CreateDateColumn, Entity, PrimaryColumn } from "typeorm";
 import { v4 as uuidv4 } from "uuid";
 
+@Entity("specifications")
 class Specification {
-  public id?: string;
-  public name: string;
-  public description: string;
-  public createdAt: Date;
+  @PrimaryColumn()
+  id?: string;
+
+  @Column()
+  name: string;
+
+  @Column()
+  description: string;
+
+  @CreateDateColumn()
+  created_at: Date;
 
   constructor() {
     this.id = this.id ? this.id : uuidv4();
