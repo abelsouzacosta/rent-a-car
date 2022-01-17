322f3619b957bf51fdfede241d4439b3f9e20f6e
Abel Souza <abelsouzacosta@gmail.com>
2022-01-17 11:12:24 -0300

    feat(specification): registering singleton

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -1,8 +1,15 @@
 import { ICategoryRepository } from "@modules/cars/repositories/category/ICategoryRepository";
 import { CategoryRepository } from "@modules/cars/repositories/implementations/category/CategoryRepository";
+import { SpecificationRepository } from "@modules/cars/repositories/implementations/specification/SpecificationRepository";
+import { ISpecificationRepository } from "@modules/cars/repositories/specification/ISpecificationRepository";
 import { container } from "tsyringe";
 
 container.registerSingleton<ICategoryRepository>(
   "CategoryRepository",
   CategoryRepository
+);
+
+container.registerSingleton<ISpecificationRepository>(
+  "SpecificationRepository",
+  SpecificationRepository
 );
