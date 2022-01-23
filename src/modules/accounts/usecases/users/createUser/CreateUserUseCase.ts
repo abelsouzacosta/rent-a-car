7ab53b153e3a2cfe5262d282c14ee8da90a1e60f
Abel Souza <abelsouzacosta@gmail.com>
2022-01-23 13:54:22 -0300

    feat(users): create
    hashin password before inserting the user instance in the database

--- src/modules/accounts/usecases/users/createUser/CreateUserUseCase.ts
+++ src/modules/accounts/usecases/users/createUser/CreateUserUseCase.ts
@@ -2,6 +2,7 @@ import {
   IUserRepository,
   ICreateUserDTO,
 } from "@modules/accounts/repositories/users/IUserRepository";
+import { hash } from "bcrypt";
 import { inject, injectable } from "tsyringe";
 
 @injectable()
@@ -25,9 +26,11 @@ class CreateUserUseCase {
 
     if (emailAlreadyTaken) throw new Error("Email already taken");
 
+    const passwordHashed = await hash(password, 8);
+
     await this.repository.create({
       name,
-      password,
+      password: passwordHashed,
       email,
       driver_license,
     });
