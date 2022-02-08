49e55a8630a8f84d2dabdf92438be9ebcf6dad29
Abel Souza <abelsouzacosta@gmail.com>
2022-02-07 21:32:52 -0300

    test(specification): create
    use case should be able to create an instance of specification

---
+++ src/modules/cars/usecases/specifications/createSpecification/CreateSpecificationUseCase.spec.ts
@@ -0,0 +1,28 @@
+import { SpecificationRepositoryInMemory } from "../../../repositories/specification/in-memory/SpecificationRepositoryInMemory";
+import { CreateSpecificationUseCase } from "./CreateSpecificationUseCase";
+
+let create: CreateSpecificationUseCase;
+let repository: SpecificationRepositoryInMemory;
+
+describe("Create Specification", () => {
+  beforeEach(() => {
+    repository = new SpecificationRepositoryInMemory();
+    create = new CreateSpecificationUseCase(repository);
+  });
+
+  it("should be able to create an category specification", async () => {
+    const specification = {
+      name: "Test Specification",
+      description: "Test description specification",
+    };
+
+    await create.execute({
+      name: specification.name,
+      description: specification.description,
+    });
+
+    const created = await repository.findByName(specification.name);
+
+    expect(created).toHaveProperty("id");
+  });
+});
