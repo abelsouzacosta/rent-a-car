a110d172fe1b4c33cf9adab19954cd0c586d934a
Abel Souza <abelsouzacosta@gmail.com>
2022-03-01 02:31:26 -0300

    test(cars): create
    adding test constraint

--- src/modules/cars/usecases/cars/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/usecases/cars/createCar/CreateCarUseCase.spec.ts
@@ -56,4 +56,22 @@ describe("Create Car Use Case", () => {
       await createCarUseCase.execute(car2);
     }).rejects.toBeInstanceOf(ApplicationError);
   });
+
+  it("should be able to create a car with avaliable equals true by default", async () => {
+    const car = {
+      name: "EcoSport",
+      description: "Suv de Entrada",
+      daily_rate: 9090,
+      license_plate: "KMO76VA",
+      fine_amount: 1230.9,
+      brand: "Ford",
+      category_id: "c2007a85-db2c-4391-96ee-0b9460e75fd2",
+    };
+
+    await createCarUseCase.execute(car);
+
+    const created = await repository.findByName(car.name);
+
+    expect(created.avaliable).toBe(true);
+  });
 });
