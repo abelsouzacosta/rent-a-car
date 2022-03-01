b761fb3f3b4cd2cd841c1d060acec19bde531bdc
Abel Souza <abelsouzacosta@gmail.com>
2022-03-01 03:22:49 -0300

    test(cars): create
    testign boundary condition

--- src/modules/cars/usecases/cars/createCar/CreateCarUseCase.spec.ts
+++ src/modules/cars/usecases/cars/createCar/CreateCarUseCase.spec.ts
@@ -90,4 +90,20 @@ describe("Create Car Use Case", () => {
       await createCarUseCase.execute(car);
     }).rejects.toBeInstanceOf(ApplicationError);
   });
+
+  it("should not be able to create a car instance with fine_amount equal or lower to zero", () => {
+    expect(async () => {
+      const car = {
+        name: "EcoSport",
+        description: "Suv de Entrada",
+        daily_rate: 300,
+        license_plate: "KMO76VA",
+        fine_amount: -100,
+        brand: "Ford",
+        category_id: "c2007a85-db2c-4391-96ee-0b9460e75fd2",
+      };
+
+      await createCarUseCase.execute(car);
+    }).rejects.toBeInstanceOf(ApplicationError);
+  });
 });
