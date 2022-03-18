a0311fec1b2cbb7066960e6af8d1feb2560b0fa1
Abel Souza <abelsouzacosta@gmail.com>
2022-03-18 01:48:01 -0300

    test(rentals): list
    adding test to ListRentalsUseCase, that list all rentals in the database

---
+++ src/modules/rentals/usecases/listRentals/ListRentalsUseCase.ts
@@ -0,0 +1,23 @@
+import { inject } from "tsyringe";
+
+import { Rental } from "@modules/rentals/infra/typeorm/entities/Rental";
+import { IRentalRepository } from "@modules/rentals/repositories/rentals/IRentalRepository";
+
+class ListRentalsUseCase {
+  private repository: IRentalRepository;
+
+  constructor(
+    @inject("RentalRepository")
+    repository: IRentalRepository
+  ) {
+    Object.assign(this, { repository });
+  }
+
+  async execute(): Promise<Rental[]> {
+    const rentals: Rental[] = [];
+
+    return rentals;
+  }
+}
+
+export { ListRentalsUseCase };
