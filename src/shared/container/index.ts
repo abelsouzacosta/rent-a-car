5a338ea29705404513764c4bfe5902c1b175ca92
Abel Souza <abelsouzacosta@gmail.com>
2022-03-24 22:05:00 -0300

    refactor(upload): adding local upload provider
    adds a instance of upload provider that uses an folder in the memory

--- src/shared/container/index.ts
+++ src/shared/container/index.ts
@@ -23,6 +23,8 @@ import { IDateProvider } from "./providers/DateProvider/IDateProvider";
 import { DayJsDateProvider } from "./providers/DateProvider/implementations/DayJsDateProvider";
 import { IMailProvider } from "./providers/MailProvider/IMailProvider";
 import { EtherealMailProvider } from "./providers/MailProvider/implementations/EtherealMailProvider";
+import { LocalStorageProvider } from "./providers/StorageProvider/implementations/LocalStorageProvider";
+import { IStorageProvider } from "./providers/StorageProvider/IStorageProvider";
 
 container.registerSingleton<ICategoryRepository>(
   "CategoryRepository",
@@ -63,6 +65,11 @@ container.registerSingleton<IRentalRepository>(
 container.registerSingleton<IUserTokenRepository>(
   "UserTokenRepository",
   UserTokenRepository
+);
+
+container.registerSingleton<IStorageProvider>(
+  "StorageProvider",
+  LocalStorageProvider
 );
 
 container.registerInstance<IMailProvider>(
