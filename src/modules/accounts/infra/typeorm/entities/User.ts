a691b306288dd2438246a6b4e65076781be9113e
Abel Souza <abelsouzacosta@gmail.com>
2022-03-27 09:40:08 -0300

    refactor(users): avatar
    adding method to recover avatar file url

--- src/modules/accounts/infra/typeorm/entities/User.ts
+++ src/modules/accounts/infra/typeorm/entities/User.ts
@@ -1,3 +1,4 @@
+import { Expose } from "class-transformer";
 import {
   Column,
   CreateDateColumn,
@@ -42,6 +43,18 @@ class User {
 
   @OneToMany(() => UserToken, (user_token) => user_token.user)
   token: UserToken[];
+
+  @Expose({ name: "avatar_url" })
+  getUserAvatarURL(): string {
+    switch (process.env.disk) {
+      case "local":
+        return `${process.env.APP_API_URL}/files/${this.avatar}`;
+      case "s3":
+        return `${process.env.AWS_BUCKET_URL}/avatar/${this.avatar}`;
+      default:
+        return null;
+    }
+  }
 
   constructor() {
     this.id = this.id ? this.id : uuidv4();
